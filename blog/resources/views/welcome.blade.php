29121f21b89729c5eadf69fca12830c4364cc97f
AmazingRoyal <tanfirulroibafi502@gmail.com>
2020-07-01 20:25:43 +0700

    MVC Pertama

---
+++ blog/resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta charset="UTF-8">
+<title>SELAMAT DATANG!</title>
+</head>
+
+<body>
+<h1>SELAMAT DATANG!</h1>
+<h3>Terima kasih telah bergabung di SanberBook. Social Media kita bersama!</h3>
+</body>
+
+</html>
