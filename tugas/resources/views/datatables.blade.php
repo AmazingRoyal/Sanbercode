61eb8c7a79268f7da3a78158fa91d27a9eb52e04
AmazingRoyal <tanfirulroibafi502@gmail.com>
2020-07-02 09:26:21 +0700

    Templating Blade

---
+++ tugas/resources/views/datatables.blade.php
@@ -0,0 +1,17 @@
+@extends('master')
+
+@section('content')
+    @include('partial.datatables_content')
+
+@endsection
+
+@push('scripts')
+    <script src=" {{ asset('/adminlte/plugins/datatables/jquery.dataTables.js') }}"></script>
+    <script src=" {{ asset('/adminlte/plugins/datatables-bs4/js/dataTables.bootstrap4.js') }}"></script>
+    
+    <script>
+    $(function () {
+        $("#example1").DataTable();
+    });
+    </script>
+@endpush
